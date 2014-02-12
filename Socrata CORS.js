bbd35cea7e3979e433b3d2ece89dc1f90b50d567
brito <francisco.brito@gmail.com>
2014-02-12 15:57:30 -0800

    [Socrata API] SoQL revenue query
    sum of revenue collected by department name

--- Socrata CORS.js
+++ Socrata CORS.js
@@ -1,18 +1,17 @@
 console.clear()
-var token = '',
-    // url='https://soda.demo.socrata.com/resource/earthquakes.json',
-    url = 'https://controllerdata.lacity.org/resource/payroll.json',
+var token = 'http://dev.socrata.com/authentication',
+    url = 'https://controllerdata.lacity.org/resource/revenue.json',
+    query = '?$select=department_name, sum(revenue_collected_1)&$group=department_name',
     xhr = new XMLHttpRequest;
     
 xhr.onload = function (){ 
     parse(JSON.parse(xhr.response)); };
     
-xhr.open('GET', url);
+xhr.open('GET', url + query);
 //xhr.setRequestHeader('X-App-Token', token);
 xhr.send();
 
 var meta = {};
 function parse(response){
     
-    console.table(response);
 }
